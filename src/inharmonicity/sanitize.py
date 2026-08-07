b6b1219d27f39388ab057f4337d9f5779e1b4ec4
Jayde Howell <jhowel62@wgu.edu>
2026-08-07 06:41:41 -0700

    refactor sanitize.py to function call

--- src/inharmonicity/sanitize.py
+++ src/inharmonicity/sanitize.py
@@ -14,26 +14,26 @@ def sanitize(wav_directory: Path) -> None:
     import_directory = wav_directory / "raw"
     export_directory = wav_directory / "sanitized"
 
-for file in import_directory.iterdir():
-    if file.suffix == ".wav":
-        sample_rate, data = sio.wavfile.read(file)
-        if sample_rate != SAMPLE_RATE:
-            raise ValueError(f"sample rate {sample_rate} does not equal the expected {SAMPLE_RATE}")
+    for file in import_directory.iterdir():
+        if file.suffix == ".wav":
+            sample_rate, data = sio.wavfile.read(file)
+            if sample_rate != SAMPLE_RATE:
+                raise ValueError(f"sample rate {sample_rate} does not equal the expected {SAMPLE_RATE}")
 
-        print(f"Processed {file.name} at sample rate {sample_rate}")
+            print(f"Processed {file.name} at sample rate {sample_rate}")
 
-        if data.ndim != 1:
-            raise ValueError(f"Expected 1D array, got {data.ndim} instead")
+            if data.ndim != 1:
+                raise ValueError(f"Expected 1D array, got {data.ndim} instead")
 
-        t_0 = np.argmax(np.abs(data))
-        start_i = t_0 + BUFFER
+            t_0 = np.argmax(np.abs(data))
+            start_i = t_0 + BUFFER
 
-        if start_i + WINDOW_SIZE > len(data):
-            raise ValueError(f"{file.name} does not contain enough samples beyond peak amplitude.")
+            if start_i + WINDOW_SIZE > len(data):
+                raise ValueError(f"{file.name} does not contain enough samples beyond peak amplitude.")
 
-        data_slice = data[start_i:start_i + WINDOW_SIZE]
+            data_slice = data[start_i:start_i + WINDOW_SIZE]
 
-        window = hann(WINDOW_SIZE, sym=False)
-        windowed_signal = data_slice * window
+            window = hann(WINDOW_SIZE, sym=False)
+            windowed_signal = data_slice * window
 
-        sio.wavfile.write(export_directory / f"{file.stem}_sanitized.wav", SAMPLE_RATE, windowed_signal.astype(np.float32))
+            sio.wavfile.write(export_directory / f"{file.stem}_sanitized.wav", SAMPLE_RATE, windowed_signal.astype(np.float32))
