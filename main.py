8a71a6ac3a3d092b936ceb573329835938eee08a
Jayde Howell <jhowel62@wgu.edu>
2026-08-08 07:55:23 -0700

    add fft function to main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from inharmonicity.sanitize import sanitize
+from inharmonicity.fft_i import get_fft
 from system.paths import get_project_root
+
+from inharmonicity.constants import WINDOW_SIZE
 
 
 def main():
@@ -11,6 +14,8 @@ def main():
     processed_count = sanitize(wav_directory)
 
     print(f"Processed {processed_count} files")
+
+    frequency_data = get_fft(wav_directory, WINDOW_SIZE)
 
 if __name__ == "__main__":
     main()
