bb4bb5927781db5e17dd2039f39ea78af8fb5360
Jayde Howell <jhowel62@wgu.edu>
2026-08-09 06:33:49 -0700

    refactor plots folder to artifacts

--- src/inharmonicity/plot.py
+++ src/inharmonicity/plot.py
@@ -7,14 +7,32 @@ def plot_frequencies(frequency_data: list[dict]):
     x_axis = [g_string["string_gauge"] for g_string in sorted_frequency_data]
     y_axis = [g_string["delta"] for g_string in sorted_frequency_data]
 
-    figure, axes = plt.subplots()
-    axes.scatter(x_axis, y_axis)
-    axes.set_xlabel("String Gauge (mm)")
-    axes.set_ylabel(r"$\Delta f$ (Hz)")
+    table_labels = [
+        "Gauge (mm)",
+        "Fundamental (Hz)",
+        "Ideal Harmonic (Hz)",
+        "Measured Harmonic (Hz)",
+        r"Delta ($\Delta f$)"
+    ]
+    table_data = [
+        [
+            f"{g_string['string_gauge']}",
+            f"{g_string['fundamental']:.2f}",
+            f"{g_string['ideal_harmonic']:.2f}",
+            f"{g_string['measured_harmonic']:.2f}",
+            f"{g_string['delta']:.2f}",
+        ]
+        for g_string in sorted_frequency_data
+    ]
 
-    axes.spines['top'].set_visible(False)
-    axes.spines['right'].set_visible(False)
-    axes.set_title("Inharmonicity Deviation on the 4th Harmonic")
+    figure_plot, axes_plot = plt.subplots()
+    axes_plot.scatter(x_axis, y_axis)
+    axes_plot.set_xlabel("String Gauge (mm)")
+    axes_plot.set_ylabel(r"$\Delta f$ (Hz)")
+
+    axes_plot.spines['top'].set_visible(False)
+    axes_plot.spines['right'].set_visible(False)
+    axes_plot.set_title("Inharmonicity Deviation on the 4th Harmonic")
 
     leastsq = np.polyfit(x_axis, y_axis, 2)
 
@@ -24,6 +42,24 @@ def plot_frequencies(frequency_data: list[dict]):
 
     ideal_y = poly_function(x_curve)
 
-    axes.plot(x_curve, ideal_y, color="black", linestyle="dashed")
+    axes_plot.plot(x_curve, ideal_y, color="black", linestyle="dashed")
 
-    figure.savefig("plots/inharmonicity_regression.pdf", format="pdf", dpi=300, bbox_inches="tight")
+    figure_plot.savefig("artifacts/inharmonicity_regression.pdf", format="pdf", dpi=300, bbox_inches="tight")
+
+    figure_table, axes_table = plt.subplots()
+
+    axes_table.axis("off")
+
+    table = axes_table.table(
+        cellText=table_data,
+        colLabels=table_labels,
+        loc="center",
+        cellLoc="center",
+        bbox=[0, 0, 1, 1],
+    )
+
+    table.auto_set_font_size(False)
+    table.set_fontsize(5)
+    table.scale(1, 1.5)
+
+    figure_table.savefig("artifacts/inharmonicity_table.pdf", format="pdf", dpi=300, bbox_inches="tight")
